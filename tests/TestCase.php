de0f802b4b74285e2ffd6fba37a0931b02f37f7d
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-06-17 15:19:48 +0200

    Configure package

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,36 +1,28 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace RalphJSmit\Livewire\Urls\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use RalphJSmit\Livewire\Urls\LivewireUrlsServiceProvider;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            LivewireUrlsServiceProvider::class,
+            LivewireServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
-        $migration->up();
-        */
+        //
     }
 }
